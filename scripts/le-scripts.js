072248f63cb9349ccc40d92611b2c9274ccb71f7
ZManak <javifuer@gmail.com>
2022-12-15 15:43:17 +0100

    added/styles

--- scripts/le-scripts.js
+++ scripts/le-scripts.js
@@ -158,19 +158,25 @@ function pintarPreguntas(lienzo){
 
                
     let card =  `<article>
+        <fieldset>
         <legend id=`+i+`>${lasPreguntas[i].pregunta}</legend>
-        
-        <label for=${lasPreguntas[i].respuestas[0]}>${lasPreguntas[i].respuestas[0]}</label>
+        <div>
         <input id=${lasPreguntas[i].respuestas[0]} type="radio" name=pregunta`+[i]+` value=${lasPreguntas[i].respuestas[0]}>
-       
-        <label for=${lasPreguntas[i].respuestas[1]}>${lasPreguntas[i].respuestas[1]}</label>
+        <label for=${lasPreguntas[i].respuestas[0]}>${lasPreguntas[i].respuestas[0]}</label>
+        </div>
+        <div>
         <input id=${lasPreguntas[i].respuestas[1]} type="radio" name=pregunta`+[i]+` value=${lasPreguntas[i].respuestas[1]}>
-        
-        <label for=${lasPreguntas[i].respuestas[2]}>${lasPreguntas[i].respuestas[2]}</label>
+        <label for=${lasPreguntas[i].respuestas[1]}>${lasPreguntas[i].respuestas[1]}</label>
+        </div>
+        <div>
         <input id=${lasPreguntas[i].respuestas[2]} type="radio" name=pregunta`+[i]+` value=${lasPreguntas[i].respuestas[2]}>
-        
-        <label for=${lasPreguntas[i].respuestas[3]}>${lasPreguntas[i].respuestas[3]}</label>
+        <label for=${lasPreguntas[i].respuestas[2]}>${lasPreguntas[i].respuestas[2]}</label>
+        </div>
+        <div>
         <input id=${lasPreguntas[i].respuestas[3]} type="radio" name=pregunta`+[i]+` value=${lasPreguntas[i].respuestas[3]}>
+        <label for=${lasPreguntas[i].respuestas[3]}>${lasPreguntas[i].respuestas[3]}</label>
+        </div>
+        </fieldset>
     </article>`
     
         div.innerHTML = card
